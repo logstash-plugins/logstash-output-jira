d98b3304d47e917909eb58b55fd336f685bade52
Juri Hudolejev <jhudolejev@gmail.com>
2015-06-04 14:47:14 +0400

    Improve documentation

--- lib/logstash/outputs/jira.rb
+++ lib/logstash/outputs/jira.rb
@@ -1,21 +1,63 @@
 # encoding: utf-8
-# Origin https://groups.google.com/forum/#!msg/logstash-users/exgrB4iQ-mw/R34apku5nXsJ
-# and https://botbot.me/freenode/logstash/msg/4169496/ 
-# via https://gist.github.com/electrical/4660061e8fff11cdcf37#file-jira-rb
-
-# Uses jiralicious as the bridge to JIRA
-# By Martin Cleaver, Blended Perspectives
-# with a lot of help from 'electrical' in #logstash
 
 require "logstash/outputs/base"
 require "logstash/namespace"
 
-
+# This output allows you to use Logstash to parse and structure
+# your logs and ship structured event data to JIRA.
 #
-# This is so is most useful so you can use logstash to parse and structure
-# your logs and ship structured, json events to JIRA
+# Structured event data will be added to the JIRA issue as 'Description' field value.
 #
-# To use this, you'll need to ensure your JIRA instance allows REST calls
+# Example JSON-encoded event:
+#
+#     {
+#       "message": "Hello JIRA!",
+#       "@version": "1",
+#       "@timestamp": "2015-06-04T10:23:30.279Z",
+#       "type": "syslog",
+#       "host": "192.168.1.42",
+#       "syslog_pri": "11",
+#       "syslog_timestamp": "Jun  4 14:23:30",
+#       "syslog_host": "myhost",
+#       "program": "root",
+#       "syslog_severity_code": 3,
+#       "syslog_facility_code": 1,
+#       "syslog_facility": "user-level",
+#       "syslog_severity": "error"
+#     }
+#
+# Example JIRA issue created the event above:
+#
+#     Type:        Task
+#     Priority:    2 - Major
+#     Status:      TO DO
+#     Resolution:  Unresolved
+#     Summary:     [logstash] Hello JIRA!
+#     Description:
+#         ---
+#         message: Hello JIRA!
+#         '@version': '1'
+#         '@timestamp': 2015-06-04 10:23:30.279000000 Z
+#         type: syslog
+#         host: 192.168.1.42
+#         syslog_pri: '11'
+#         syslog_timestamp: Jun 4 14:23:30
+#         syslog_host: myhost
+#         program: root
+#         syslog_severity_code: 3
+#         syslog_facility_code: 1
+#         syslog_facility: user-level
+#         syslog_severity: error
+#
+# To use this output you'll need to ensure that your JIRA instance allows REST calls.
+#
+# This output uses `jiralicious` as the bridge to JIRA
+# By Martin Cleaver, Blended Perspectives
+# with a lot of help from 'electrical' in #logstash.
+#
+# Origin <https://groups.google.com/forum/#!msg/logstash-users/exgrB4iQ-mw/R34apku5nXsJ>
+# and <https://botbot.me/freenode/logstash/msg/4169496/>
+# via <https://gist.github.com/electrical/4660061e8fff11cdcf37#file-jira-rb>.
 
 class LogStash::Outputs::Jira < LogStash::Outputs::Base
   config_name "jira"
@@ -36,6 +78,8 @@ class LogStash::Outputs::Jira < LogStash::Outputs::Base
   config :issuetypeid, :validate => :string, :required => true
 
   # JIRA Summary
+  #
+  # Truncated and appended with '...' if longer than 255 characters.
   config :summary, :validate => :string, :required => true
 
   # JIRA Priority
